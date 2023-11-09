13e883122f60b0b5ce42c7646349b9695c58f59d
2055619 <2055619@claurendeau.qc.ca>
2023-11-09 18:03:22 -0500

    Accordion à fix

--- src/components/games/MiniGame.tsx
+++ src/components/games/MiniGame.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GameHeader from "./GameHeader";
 import {useNavigate} from "react-router-dom";
 import GameFooter from "./GameFooter";
+import { Accordion, Card, Button, Form } from 'react-bootstrap';
 
 function MiniGame() {
     const navigate = useNavigate();
@@ -12,58 +13,71 @@ function MiniGame() {
             <GameHeader />
             <div className="row pt-4 p-2 bg-white">
                 <div className="col">
-                    <div id="accordion">
-                        <div className="accordion-item">
-                            <h5 className="accordion-header" id="header-connex">
-                                <button className="btn btn-outline-primary" data-bs-toggle="collapse"
-                                        data-bs-target="#connex" aria-expanded="true" aria-controls="connex">
-                                    Connexion </button>
-                            </h5>
+                    <Accordion defaultActiveKey="0">
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Button as={Button} variant="outline-primary" eventKey="0">
+                                    Connexion
+                                </Accordion.Button>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body>
+                                    <Form className="mb-4" autoComplete="off">
+                                        <Form.Group className="mb-3" controlId="user">
+                                            <Form.Label>Utilisateur</Form.Label>
+                                            <Form.Control type="text" placeholder="Entrer votre utilisateur" />
+                                        </Form.Group>
 
-                            <div id="connex" className="collapse show border rounded p-3 ml-5 mr-5 mt-4" aria-labelledby="header-connex" data-bs-parent="#accordion">
-                                <form className="mb-4" autoComplete="off">
-                                    <label className="col-12 m-auto" htmlFor="user">Utilisateur</label>
-                                    <input className="champ col-12" type="text" id="user" placeholder="Entrer votre utilisateur" />
+                                        <Form.Group className="mb-3" controlId="mdp">
+                                            <Form.Label>Mot de passe</Form.Label>
+                                            <Form.Control type="password" placeholder="" />
+                                        </Form.Group>
 
-                                    <label className="col-12 mt-2 mb-0" htmlFor="mdp">Mot de passe</label>
-                                    <input className="champ col-12" type="password" id="mdp" placeholder="" />
+                                        <div className="text-center mt-4 mb-2">
+                                            <Button variant="success" className="me-2" onClick={() => { /* Add your logic here */ }}>
+                                                Se Connecter
+                                            </Button>
+                                            <a href="/jeux/maintenance" style={{ fontSize: '12px' }}>Mot de passe oublié?</a>
+                                        </div>
+                                    </Form>
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
 
-                                    <div className="col text-center mt-4 mb-2">
-                                        <button className="btn btn-success me-2" id="btnCo" onClick={() => {navigate('/MiniGame/catalogue')}}>
-                                            Se Connecter</button>
-                                        <a href="/jeux/maintenance" style={{ fontSize: '12px' }}>mot de passe oublié?</a>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Button as={Button} variant="outline-primary" eventKey="1">
+                                    Inscription
+                                </Accordion.Button>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="1">
+                                <Card.Body>
+                                    <Form autoComplete="off">
+                                        <Form.Group className="mb-3" controlId="userIn">
+                                            <Form.Label>Utilisateur</Form.Label>
+                                            <Form.Control type="text" placeholder="Choisir un nom d'utilisateur" />
+                                        </Form.Group>
 
-                        <div className="accordion-item">
-                            <h5 className="accordion-header mt-3" id="header-ins">
-                                <a className="btn btn-outline-primary" data-bs-toggle="collapse" data-bs-target="#ins" aria-expanded="true" aria-controls="ins">Inscription</a>
-                            </h5>
+                                        <Form.Group className="mb-3" controlId="pseudo">
+                                            <Form.Label>Pseudonyme</Form.Label>
+                                            <Form.Control type="text" placeholder="Choisir un pseudonyme" />
+                                        </Form.Group>
 
-                            <div id="ins" className="collapse border rounded p-3 ml-5 mr-5 mt-4" aria-labelledby="header-ins" data-bs-parent="#accordion">
-                                <form autoComplete="off">
-                                    <label className="col-12 m-auto" htmlFor="userIn">Utilisateur</label>
-                                    <input className="champ col-12" type="text" id="userIn" placeholder="Choisir un nom d'utilisateur" />
+                                        {/* Add other form groups for password and confirmation here */}
 
-                                    <label className="col-12 m-auto" htmlFor="pseudo">Pseudonyme</label>
-                                    <input className="champ col-12" type="text" id="pseudo" placeholder="Choisir un pseudonyme" />
-
-                                    <label className="col-12 mt-2 mb-0" htmlFor="mdpIn">Mot de passe</label>
-                                    <input className="champ col-12" type="password" id="mdpIn" placeholder="Mot de passe" />
-
-                                    <label className="col-12 mt-2 mb-0" htmlFor="mdpInConf">Confirmation du mot de passe</label>
-                                    <input className="champ col-12" type="password" id="mdpInConf" placeholder="Confirmation du Mot de passe" />
-
-                                    <div className="col text-center mt-4">
-                                        <input className="btn btn-success me-1" type="button" value="S'inscrire" id="btnInscrip" onClick={() => {}} />
-                                        <input className="btn btn-danger ms-1" type="reset" value="Annuler" id="btnAnnule" />
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+                                        <div className="text-center mt-4">
+                                            <Button variant="success" className="me-1" type="button" onClick={() => { /* Add your logic here */ }}>
+                                                S'inscrire
+                                            </Button>
+                                            <Button variant="danger" className="ms-1" type="reset">
+                                                Annuler
+                                            </Button>
+                                        </div>
+                                    </Form>
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
                 </div>
             </div>
 
