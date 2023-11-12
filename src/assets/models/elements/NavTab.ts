f8c9b795fee0373bcd49adfd12ffc75cae33363c
2055619 <2055619@claurendeau.qc.ca>
2023-11-12 10:37:40 -0500

    New nav départ

--- src/assets/models/elements/NavTab.ts
+++ src/assets/models/elements/NavTab.ts
@@ -1,3 +1,5 @@
+import {Nav} from "react-bootstrap";
+import {useState} from "react";
 
 export interface INavTab {
     name: string;
@@ -14,3 +16,24 @@ class NavTab implements INavTab {
     }
 }
 export default NavTab;
+
+// const navItems = [
+//     new NavTab('home', 'header.home'),
+//     new NavTab('intro', 'header.intro'),
+//     new NavTab('projet', 'header.project'),
+//     new NavTab('progres', 'header.progress'),
+//     new NavTab('contact', 'header.contact')
+// ]
+// const [isHover, setIsHover] = useState(false);
+//
+// <Nav className="justify-content-between ">
+//     {
+//         navItems.map((navItem: INavTab, index: number) => (
+//             <Nav.Item key={index}>
+//             <Nav.Link className={`px-3 ${isHover?'active':''}`} href={`/portfolio#${navItem.name}`} onClick={() => navigate(`/portfolio#${navItem.name}`)}>
+// {t(navItem.label)}
+// </Nav.Link>
+// </Nav.Item>
+// ))
+// }
+// </Nav>
