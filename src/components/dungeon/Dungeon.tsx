00c75cfaaa41354a57b35d96ec331688debdb511
2055619 <2055619@claurendeau.qc.ca>
2023-11-14 15:45:59 -0500

    BottomBar.tsx

--- src/components/dungeon/Dungeon.tsx
+++ src/components/dungeon/Dungeon.tsx
@@ -3,8 +3,11 @@ import SideBar from "./side/SideBar";
 import {useState} from "react";
 import Character from "./personnage/Character";
 import Store from "./Store";
+import BottomBar from "./BottomBar";
+import {useTranslation} from "react-i18next";
 
 function Dungeon() {
+    const {t} = useTranslation();
     const [tab, setTab] = useState<string>('character')
 
     return (
@@ -23,16 +26,16 @@ function Dungeon() {
             <section className="container-fluid g-0">
                 <div className="col-xl-9 col-lg-8 col-md-8 ms-auto g-0">
                     <div className="row my-md-2 my-4"></div>
-                    {tab === 'character' && <Character />}
-                    {tab === 'store' && <Store />}
-                    {tab === 'compendium' && <h1>Compendium</h1>}
-                    {tab === 'inventory' && <h1>Inventory</h1>}
-                    {tab === 'map' && <h1>Map</h1>}
-                    {tab === 'administration' && <h1>Administration</h1>}
+                    {tab === 'character' && <Character/>}
+                    {tab === 'store' && <Store/>}
+                    {tab === 'compendium' && <h1>{t('pages.dungeon.compendium')}</h1>}
+                    {tab === 'inventory' && <h1>{t('pages.dungeon.inventory')}</h1>}
+                    {tab === 'map' && <h1>{t('pages.dungeon.map')}</h1>}
+                    {tab === 'administration' && <h1>{t('pages.dungeon.administration')}</h1>}
                 </div>
 
             </section>
-            {/*    TODO: Bottom bar*/}
+            {/*<BottomBar character={}/>*/}
         </div>
     )
 }
