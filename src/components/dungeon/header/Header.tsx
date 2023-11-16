9e46b7b76553c348ac75927c924ced85c1ba14fa
2055619 <2055619@claurendeau.qc.ca>
2023-11-16 17:38:51 -0500

    Meilleur Header

--- src/components/dungeon/header/Header.tsx
+++ src/components/dungeon/header/Header.tsx
@@ -1,8 +1,13 @@
-import Name from "./Name";
 import HealthBar from "./HealthBar";
+import {Form, FormSelect} from "react-bootstrap";
+import {ICharactere} from "../../../assets/models/dungeon/character";
+import {useNavigate} from "react-router-dom";
+import React, {useState} from "react";
 
+function Header({character, characters, changeCharacter}: {character: ICharactere, characters: ICharactere[], changeCharacter: any}){
+    const navigate = useNavigate();
+    const [disable, setDisable] = useState(true);
 
-function Header(){
     return(
         <div className="row fixed-top mt-5 clearfix bg-dark align-items-center pt-3 pb-1">
             <div className="col-2 col-sm-1 d-md-none">
@@ -12,12 +17,24 @@ function Header(){
                     <span className="navbar-toggler-icon"></span>
                 </button>
             </div>
+            {/*TODO: Change to put on the left */}
             <div className="col-xl-4 col-lg-5 col-md-5 ms-auto col-sm-11 col-10">
-                <Name/>
+                <div className="row text-light">
+                    <a className="col-1 ms-2 " onClick={() => navigate('/portfolio')}><i
+                        className="fa-sharp fa-solid fa-house-user fa-3x text-warning"></i></a>
+                    <Form className={`col-6`}>
+                        <FormSelect className="form-select badge w-50 bg-light text-dark ms-5 mt-1"
+                                    aria-label="Default select example" onChange={changeCharacter} defaultValue={character.id}>
+                            {
+                                characters.map((character: ICharactere) => {
+                                    return <option value={character.id} className="fw-bold">{character.name}</option>
+                                })
+                            }
+                        </FormSelect>
+                    </Form>
+                </div>
             </div>
-            <div className="col-xl-8 col-lg-7 col-md-7">
-                <HealthBar/>
-            </div>
+            <HealthBar character={character}/>
         </div>
     )
 }
--- src/components/dungeon/header/HealthBar.tsx
+++ src/components/dungeon/header/HealthBar.tsx
@@ -1,32 +1,35 @@
 import React, { useState } from 'react';
+import {ICharactere} from "../../../assets/models/dungeon/character";
 
-function HealthBar() {
+function HealthBar({ character }: { character: ICharactere}) {
     const [disable, setDisable] = useState(true);
 
     return (
-        <div className="d-flex align-items-center">
-            <button
-                className="btn btn-danger mx-2 fw-bold"
-                // onClick={decrementHealth}
-                disabled={disable}
-            >-</button>
-            <div className="col text-center">
-                <div className="progress" style={{ height: '25px' }}>
-                    <div
-                        // onClick={decrementHealth} ${getCouleur()}
-                        className={`progress-bar fw-bold `}
-                        role="progressbar"
-                        // style={{ width: `${(100 * myCharacter.currentHealth / myCharacter.health)}%` }}
-                        // aria-valuenow="25"
-                        // aria-valuemin="0" aria-valuemax="100"
-                    >EDIT: CHAR_HEALTH</div>
+        <div className="col-xl-8 col-lg-7 col-md-7">
+            <div className="d-flex align-items-center">
+                <button
+                    className="btn btn-danger mx-2 fw-bold"
+                    // onClick={decrementHealth}
+                    disabled={character.currentHealth === 0}
+                >-</button>
+                <div className="col text-center">
+                    <div className="progress" style={{ height: '25px' }}>
+                        <div
+                            // onClick={decrementHealth} ${getCouleur()}
+                            className={`progress-bar fw-bold `}
+                            role="progressbar"
+                            style={{ width: `${(100 * character.currentHealth / character.health)}%` }}
+                            // aria-valuenow="25"
+                            // aria-valuemin="0" aria-valuemax="100"
+                        >EDIT: CHAR_HEALTH</div>
+                    </div>
                 </div>
+                <button
+                    className="btn btn-success mx-2 fw-bold"
+                    // onClick={incrementHealth}
+                    disabled={character.currentHealth === character.health}
+                >+</button>
             </div>
-            <button
-                className="btn btn-success mx-2 fw-bold"
-                // onClick={incrementHealth}
-                disabled={disable}
-            >+</button>
         </div>
     );
 }
