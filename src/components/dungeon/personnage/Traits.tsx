6fa1127a3d5ec9157e1848233467b55d67d9c0fd
2055619 <2055619@claurendeau.qc.ca>
2023-11-16 19:54:58 -0500

    Traits

--- src/components/dungeon/personnage/Traits.tsx
+++ src/components/dungeon/personnage/Traits.tsx
@@ -1,8 +1,21 @@
+import {Card} from "react-bootstrap";
+import {useTranslation} from "react-i18next";
+
 function Traits() {
+    const {t} = useTranslation();
     return (
-        <div>
-            <h1>Traits</h1>
-        </div>
+        <>
+            <h3 className="text-white text-center mb-3 mt-4">{t('pages.dungeon.traits')}</h3>
+            <div className="overflow-auto traits-personnage">
+                {/*TODO: Loop sur chaque*/}
+                <Card className="card mt-3">
+                    <Card.Body >
+                        <Card.Title>name</Card.Title>
+                        <Card.Text>desc</Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        </>
     )
 }
 
