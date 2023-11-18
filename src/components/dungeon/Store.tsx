1f43c0d3809ccef624fec0f96de2f1e73d82fbe9
2055619 <2055619@claurendeau.qc.ca>
2023-11-17 20:16:54 -0500

    start dragNdrop

--- src/components/dungeon/Store.tsx
+++ src/components/dungeon/Store.tsx
@@ -2,14 +2,71 @@ import {useTranslation} from "react-i18next";
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {IEquipDnd, IEquipments} from "../../assets/models/dungeon/equipments";
 import {dndService} from "../../App";
 
 function Store() {
     const {t} = useTranslation();
-    const [equipments, setEquipments] = useState<IEquipDnd[]>();
-    const [selectedEquipment, setSelectedEquipment] = useState<IEquipments>();
+    const dragItem = useRef();
+    const dragOverItem = useRef();
+
+    const [equipments, setEquipments] = useState<IEquipDnd[]>(
+        [{
+            index: '',
+            name: '',
+            url: ''
+        }]);
+    const [equipmentsSelected, setEquipmentsSelected] = useState<IEquipments[]>(
+        [{
+            index: "chain-mail",
+            name: "Chain Mail",
+            desc: ["A set of interlocking metal rings, offering good protection."],
+            equipment_category: {
+                index: "armor",
+                name: "Armor",
+                url: "/api/equipment-categories/armor",
+            },
+            armor_category: "Heavy",
+            armor_class: {
+                base: 16,
+                dex_bonus: false,
+            },
+            str_minimum: 13,
+            stealth_disadvantage: true,
+            weight: 55,
+            cost: {
+                quantity: 75,
+                unit: "gp",
+            },
+            url: "/api/equipment/chain-mail",
+        }]
+    );
+    const [selectedEquipment, setSelectedEquipment] = useState<IEquipments>(
+        {
+            index: "chain-mail",
+            name: "Chain Mail",
+            desc: ["A set of interlocking metal rings, offering good protection."],
+            equipment_category: {
+                index: "armor",
+                name: "Armor",
+                url: "/api/equipment-categories/armor",
+            },
+            armor_category: "Heavy",
+            armor_class: {
+                base: 16,
+                dex_bonus: false,
+            },
+            str_minimum: 13,
+            stealth_disadvantage: true,
+            weight: 55,
+            cost: {
+                quantity: 75,
+                unit: "gp",
+            },
+            url: "/api/equipment/chain-mail",
+        }
+    );
 
     useEffect(() => {
         dndService.getAllEquipments().then((response) => {
@@ -18,35 +75,55 @@ function Store() {
     }, []);
 
     function getEquip(url: string) {
-        if (equipments) {
-            dndService.getEquipmentByUrl(url).then((response) => {
-                setSelectedEquipment(response);
-            });
-        }
+        dndService.getEquipmentByUrl(url).then((response) => {
+            setSelectedEquipment(response);
+        });
     }
 
+    const dragStart = (e: any, position: any) => {
+        dragItem.current = position;
+    };
+
+    const dragEnter = (e: any, position: any) => {
+        dragOverItem.current = position;
+    };
+
+    const drop = (e: any) => {
+        const copyListItems = [...equipments];
+        const dragItemContent = copyListItems[dragItem.current!];
+        copyListItems.splice(dragItem.current!, 1);
+        copyListItems.splice(dragOverItem.current!, 0, dragItemContent);
+        // dragItem.current = null;
+        // dragOverItem.current = null;
+        setEquipments(copyListItems);
+    };
+
     return (
-        <div className="container-fluid bg-warning p-5 vh-100">
+        <div className="container-fluid bg-warning p-5 min-vh-100">
             <h1 className="display-1">{t('pages.dungeon.store')}</h1>
             <div className="row">
                 <div className="bg-light row pb-4">
                     <Accordion className="col-5">
                         <h1 className="display-5">{t('pages.dungeon.newEquipments')}</h1>
                         {
-                            equipments?.map((equip: IEquipDnd, index) => {
+                            equipments?.filter((equip) => !equipmentsSelected.includes(selectedEquipment)).map((equip: IEquipDnd, index) => {
                                 return (
                                     <Accordion.Item eventKey={index.toString()} key={index}>
                                         <Card>
                                             <Card.Header className={"m-0 p-0"}>
                                                 <Accordion.Button as={Button} variant="link"
                                                                   onClick={() => getEquip(equip.url)}
-                                                                  eventKey={index.toString()}>
+                                                                  eventKey={index.toString()}
+                                                                  onDragStart={(e) => dragStart(e, index)}
+                                                                  onDragEnter={(e) => dragEnter(e, index)}
+                                                                  onDragEnd={drop}
+                                                                  draggable>
                                                     {equip.name}
                                                 </Accordion.Button>
                                             </Card.Header>
                                             <Accordion.Collapse eventKey={index.toString()}>
                                                 <Card.Body>
-                                                    {selectedEquipment?.desc && selectedEquipment?.desc?.length > 0 ? selectedEquipment?.desc : t('pages.dungeon.noDescription')}
+                                                    {selectedEquipment?.desc && selectedEquipment.desc.length > 0 ? selectedEquipment.desc : t('pages.dungeon.noDescription')}
                                                 </Card.Body>
                                             </Accordion.Collapse>
                                         </Card>
@@ -59,7 +136,13 @@ function Store() {
                         <div className="position-sticky sticky-top" style={{top: '15%'}}>
                             <h1 className="display-5">{t('pages.dungeon.addInventory')}</h1>
                             <ul className="">
-                                <li className={"h4"}>ED: eq.Name</li>
+                                {
+                                    equipmentsSelected?.map((equip: IEquipments, index) => {
+                                        return (
+                                            <li className={"h4"} key={index}>{equip.name}</li>
+                                        )
+                                    })
+                                }
                             </ul>
                             <Button className="" type="submit">{t('pages.dungeon.confirmAdd')}</Button>
                         </div>
