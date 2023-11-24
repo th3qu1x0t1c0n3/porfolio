e6e8729a0f9db4e0c6b9505862c06efd0ea4337a
2055619 <2055619@claurendeau.qc.ca>
2023-11-24 11:20:14 -0500

    Traits Fait

--- src/components/dungeon/personnage/Traits.tsx
+++ src/components/dungeon/personnage/Traits.tsx
@@ -3,39 +3,48 @@ import {useTranslation} from "react-i18next";
 import {useEffect, useState} from "react";
 import {ICharactere, ITrait} from "../../../assets/models/dungeon/character";
 import {dndService, personnageService} from "../../../App";
+import {toast} from "react-toastify";
 
 interface ITraitsComponent {
     character: ICharactere
 }
+
 function Traits({character}: ITraitsComponent) {
     const {t} = useTranslation();
     const [traits, setTraits] = useState<ITrait[]>([]);
 
-    // useEffect(() => {
-    //     personnageService.getTrait(character.id).then((response) => {
-    //         if (response === undefined) return;
-    //         response.map((trait) => {
-    //             dndService.getTrait(trait.reference).then((response) => {
-    //                 if (response === undefined) return;
-    //                 setTraits(traits => [...traits, response]);
-    //             })
-    //         })
-    //     }).catch((error) => {
-    //
-    //     })
-    // }, []);
+    useEffect(() => {
+        let newTraits: ITrait[] = [];
+        personnageService.getTrait(character.id).then((response) => {
+            if (response === undefined) return;
+            response.map((traitRef) => {
+                dndService.getTrait(traitRef.trait!).then((response) => {
+                    if (response === undefined) return;
+                    if (traits.includes(response)) return;
+                    if (response.name === undefined) return;
+                    newTraits.push(response);
+                })
+            })
+            setTraits(newTraits);
+        }).catch((error) => {
+            toast.error(error);
+        })
+    }, []);
 
     return (
         <>
             <h3 className="text-white text-center mb-3 mt-4">{t('pages.dungeon.traits')}</h3>
             <div className="overflow-auto traits-personnage">
-                {/*TODO: Loop sur chaque*/}
-                <Card className="card mt-3">
-                    <Card.Body >
-                        <Card.Title>name</Card.Title>
-                        <Card.Text>desc</Card.Text>
-                    </Card.Body>
-                </Card>
+                {
+                    traits.map((trait, index) => {
+                        return <Card className="card mt-3" key={index}>
+                            <Card.Body >
+                                <Card.Title>{trait.name}</Card.Title>
+                                <Card.Text>{trait.desc}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    })
+                }
             </div>
         </>
     )
