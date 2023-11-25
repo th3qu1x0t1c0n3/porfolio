1e1ac849f5803312e86bd2caf8e2dc53c61cc014
2055619 <2055619@claurendeau.qc.ca>
2023-11-25 11:25:42 -0500

    Ajout BE

---
+++ porfolio/src/main/java/quixotic/personnal/porfolio/PorfolioApplication.java
@@ -0,0 +1,13 @@
+package quixotic.personnal.porfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PorfolioApplication.class, args);
+    }
+
+}
---
+++ porfolio/src/test/java/quixotic/personnal/porfolio/PorfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package quixotic.personnal.porfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
