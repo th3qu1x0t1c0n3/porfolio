150c13f2cbbb6f07e038eba9704da981c94ad78e
2055619 <2055619@claurendeau.qc.ca>
2023-11-27 14:37:32 -0500

    Progression

--- porfolio_fe/src/components/home/Progression.tsx
+++ porfolio_fe/src/components/home/Progression.tsx
@@ -2,6 +2,23 @@ import {useTranslation} from "react-i18next";
 
 function Progression(){
     const { t } = useTranslation();
+    const progress = [
+        {name: 'JAVA', value: 95},
+        {name: 'SQL', value: 50},
+        {name: 'ANGULAR', value: 90},
+        {name: 'React', value: 90},
+        {name: 'C++', value: 10},
+    ];
+
+    function getColor(pct: number) {
+        if(pct < 30) return 'bg-danger';
+        if(pct <= 50) return 'bg-warning';
+        if(pct < 80) return 'bg-success';
+        if(pct <= 90) return 'bg-info';
+        if(pct < 100) return 'bg-primary';
+
+        return 'bg-light';
+    }
 
     return(
         <section id="progres" className="p-5 bg-white">
@@ -14,36 +31,48 @@ function Progression(){
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-lg-6 col-sm-10 text-secondary">
-                        <h2>JAVA</h2>
-                        <div className="progress bg-secondary mb-3">
-                            <div className="progress-bar" style={{width: '95%'}}>
-                                95%
-                            </div>
-                        </div>
-                        <h2>SQL</h2>
-                        <div className="progress bg-secondary mb-3">
-                            <div className="progress-bar bg-danger" style={{width: '50%'}}>
-                                50%
-                            </div>
-                        </div>
-                        <h2>ANGULAR</h2>
-                        <div className="progress bg-secondary mb-3">
-                            <div className="progress-bar bg-warning" style={{width: '90%'}}>
-                                90%
-                            </div>
-                        </div>
-                        <h2>React</h2>
-                        <div className="progress bg-secondary mb-3">
-                            <div className="progress-bar bg-warning" style={{width: '90%'}}>
-                                90%
-                            </div>
-                        </div>
-                        <h2>C++</h2>
-                        <div className="progress bg-secondary mb-3">
-                            <div className="progress-bar bg-success" style={{width: '10%'}}>
-                                10%
-                            </div>
-                        </div>
+                        {
+                            progress.map((item, index) => (
+                                <div key={index}>
+                                    <h2>{item.name}</h2>
+                                    <div className={`progress mb-3 bg-secondary`}>
+                                        <div className={`progress-bar ${getColor(item.value)}`} style={{width: item.value + '%'}}>
+                                            {item.value}%
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                        {/*<h2>JAVA</h2>*/}
+                        {/*<div className="progress bg-secondary mb-3">*/}
+                        {/*    <div className="progress-bar" style={{width: '95%'}}>*/}
+                        {/*        95%*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
+                        {/*<h2>SQL</h2>*/}
+                        {/*<div className="progress bg-secondary mb-3">*/}
+                        {/*    <div className="progress-bar bg-danger" style={{width: '50%'}}>*/}
+                        {/*        50%*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
+                        {/*<h2>ANGULAR</h2>*/}
+                        {/*<div className="progress bg-secondary mb-3">*/}
+                        {/*    <div className="progress-bar bg-warning" style={{width: '90%'}}>*/}
+                        {/*        90%*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
+                        {/*<h2>React</h2>*/}
+                        {/*<div className="progress bg-secondary mb-3">*/}
+                        {/*    <div className="progress-bar bg-warning" style={{width: '90%'}}>*/}
+                        {/*        90%*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
+                        {/*<h2>C++</h2>*/}
+                        {/*<div className="progress bg-secondary mb-3">*/}
+                        {/*    <div className="progress-bar bg-success" style={{width: '10%'}}>*/}
+                        {/*        10%*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
                     </div>
                 </div>
             </div>
