ac40fe1925fdc63a28c309daf48db9354aedad1f
2055619 <2055619@claurendeau.qc.ca>
2023-12-05 10:54:22 -0500

    Context

---
+++ porfolio_fe/src/components/context/DarkModeContext.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+
+const DarkModeContext = createContext();
+
+export const useDarkMode = () => useContext(DarkModeContext);
+
+export const DarkModeProvider = ({ children }) => {
+    const [darkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+        const isDarkMode = localStorage.getItem('darkMode') === 'true';
+        setDarkMode(isDarkMode);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('darkMode', darkMode);
+    }, [darkMode]);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
+
+    return (
+        <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+            {children}
+        </DarkModeContext.Provider>
+    );
+};
