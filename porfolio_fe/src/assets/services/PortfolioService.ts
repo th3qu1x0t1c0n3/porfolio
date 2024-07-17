e93ef356ddbe6f1ceea97a6949bc6d7eeaf9a768
2055619 <2055619@claurendeau.qc.ca>
2024-07-16 20:22:24 -0400

    Contact

--- porfolio_fe/src/assets/services/PortfolioService.ts
+++ porfolio_fe/src/assets/services/PortfolioService.ts
@@ -3,6 +3,8 @@ import {Message} from "../models/message";
 
 export class PortfolioService {
     async message(message: Message): Promise<string> {
+        const tmp = message;
+        message.message = "Portfolio app:\n" + message.message;
         return serverInstance.post('api/v1/utils/contact', message)
             .then((response) => {
                 return response.data;
